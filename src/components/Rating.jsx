cde6413eeb2d6388758054cc33358f859920cda1
junhyungson <cybergeron89@gmail.com>
2025-08-09 13:52:13 -0700

    added rating and feed back messages

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  // const feedbackMessages = {
+  //   1: 'very bad',
+  //   2: 'could be better',
+  //   3: 'okay',
+  //   4: 'good experience',
+  //   5: 'excellent'
+  // }
+  const feedbackMessages = ['very bad', 'could be better', 'okay', 'good', 'excellent']
   return (
-    <div style={styles.container}>
-      <h2 style={styles.heading}>Rate Experience</h2>
+    <div className="rating-container">
+      <h2>Rate Experience</h2>
+      <div className="stars">
+        {stars.map((star, index) => (
+          <span
+          className={`star ${ star <=(hover || rating) ? 'active' : '' }`}
+          onClick={() => setRating(star)}
+          onMouseEnter={() => setHover(star)}
+          onMouseLeave={() => setHover(0)}
+            key={star}
+          >{`\u2605`}</span>
+        ))}
+        {
+          rating > 0 && <p className="feedback">{feedbackMessages[rating -1]}</p>
+        }
+      </div>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    textAlign: "center",
-    fontFamily: "Arial",
-    padding: "20px",
-  },
-  heading: {
-    color: "red",
-  },
 };
 
 export default Rating;
