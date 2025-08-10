b871aa38df194c341a17e879b5887d03ad7e9415
junhyungson <cybergeron89@gmail.com>
2025-08-10 08:11:19 -0700

    adding custom feedbackmessages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import Rating from "./components/Rating";
 const App = () => {
   return (
     <div>
+      <Rating
+        heading="different type of rating header here"
+        color="red"
+        feedbackMessages={["bad", "meh", "ok", "good", "fine"]}
+      />
+      <Rating heading="another" />
       <Rating />
     </div>
   );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,34 +1,40 @@
 import { useState } from "react";
 
-const Rating = () => {
+const Rating = ({
+  heading = "Rate your experience",
+  color = "gold",
+  feedbackMessages = [
+    "very bad",
+    "could be better",
+    "okay",
+    "good",
+    "excellent",
+  ],
+}) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
-  // const feedbackMessages = {
-  //   1: 'very bad',
-  //   2: 'could be better',
-  //   3: 'okay',
-  //   4: 'good experience',
-  //   5: 'excellent'
-  // }
-  const feedbackMessages = ['very bad', 'could be better', 'okay', 'good', 'excellent']
   return (
     <div className="rating-container">
-      <h2>Rate Experience</h2>
+      <h2>{heading}</h2>
       <div className="stars">
         {stars.map((star, index) => (
           <span
-          className={`star ${ star <=(hover || rating) ? 'active' : '' }`}
-          onClick={() => setRating(star)}
-          onMouseEnter={() => setHover(star)}
-          onMouseLeave={() => setHover(0)}
+            // className={`star ${ star <=(hover || rating) ? 'active' : '' }`}
+            className="star"
+            style={{
+              color: star <= (hover || rating) ? color : "#ccc",
+            }}
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
             key={star}
           >{`\u2605`}</span>
         ))}
-        {
-          rating > 0 && <p className="feedback">{feedbackMessages[rating -1]}</p>
-        }
+        {rating > 0 && (
+          <p className="feedback">{feedbackMessages[rating - 1]}</p>
+        )}
       </div>
     </div>
   );
